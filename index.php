be8e061a3412fbf0362c6d5afc27de02ba492a5d
PavelCherepanov <pavelk.cherepanov@gmail.com>
2021-03-14 21:33:35 +0700

    first commit

--- index.php
+++ index.php
@@ -1,42 +1,29 @@
-<?php 
-	$error = "";
-	$successMessage = "";
+<?php
+// $email = "fle43016@cuoly.com";
 
-	if ($_POST){
-		
-		if(!$_POST["email"]){
-			$error .= "Email обязателен<br>";
-		}
+if ($_GET["city"]){
 
-		if(!$_POST["subject"]){
-			$error .= "Тема обязателена<br>";
-		}
+  $urlContent =  file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=".urlencode($_GET["city"])."&appid=YOURAPI"); 
 
-		if(!$_POST["text_mess"]){
-			$error .= "Текст сообщения обязателен<br>";
-		}
+  $weatherArray = json_decode($urlContent, true);
+  print_r($weatherArray);
+  
+  if ($weatherArray["cod"] == 200){
+    $weather = "The weather in ".$_GET['city']." is currently '".$weatherArray['weather'][0]['description']."'.";
 
-		if (!$_POST["email"] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-  			$error .= "Email не верный<br>";
-		}
+    $tempInCelcius = intval($weatherArray['main']['temp'] - 273);
 
-		if ($error != ""){
-			$error = '<div class="alert alert-danger" role="alert"><p>У вас ошибка:</p>' . $error . '</div>';
-		} else{
-			$emailto = $_POST["email"];
-			$subject = $_POST["subject"];
-			$text = $_POST["text_mess"];
-			$headers = "From: ".$_POST["email"];
+    $weather .= "The temperature ".$tempInCelcius."&deg";
 
-			if (mail($emailTo, $subject, $text, $headers)){
-				$successMessage = '<div class="alert alert-success" role="alert"><p>Ваше сообщение было отправлено</p></div>';
-			} else {
-				$error = '<div class="alert alert-danger" role="alert"><p>Ваше сообщение не было отправлено</p></div>';
-			}
-		}
-	}
+    $wind = $weatherArray['wind']['speed'];
+
+    $weather .= " Wind speed is ".$wind."m/s.";
+} else {
+    $error .= "City not found.";
+  }
+
+}
 ?>
-
 
 <!doctype html>
 <html lang="en">
@@ -46,70 +33,70 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
+    <title>Weather</title>
 
-    <title>Send Email</title>
+
   </head>
   <body>
-  	<div class="container">
-    	<h1>Напиши свою почту!</h1>
-    	<div id="error"><? echo $error.$successMessage; ?></div>
-		<form method="post">
-		  <div class="mb-3">
-		    <label for="email" class="form-label">Email</label>
-		    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" name="email">
-		    <div id="emailHelp" class="form-text">Мы никому не скажем ваш email.</div>
-		  </div>
-		  <div class="mb-3">
-		    <label for="subject" class="form-label">Тема</label>
-		    <input type="text" class="form-control" id="subject" name="subject">
-		  </div>
-		  <div class="mb-3">
-		    <label for="text_mess" class="form-label">Текст сообщения</label>
-		    <textarea id="text_mess" class="form-control" rows="3" name="text_mess"></textarea>
-		  </div>
-		  <button type="submit" class="btn btn-primary">Отправить</button>
-		</form>
 
+    <style type="text/css">
+      html { 
+        background: url("background_image.jpg") no-repeat center center fixed; 
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+      }
+      body{
+        background: none;
+      }
 
+      .container{
+        text-align: center;
+        margin-top: 100px;
+        width:  450px;
+      }
 
+      input{
+        margin: 20px 0;
+      }
+
+      #weather{
+        margin-top: 15px;
+      }
+    </style>
+    <div class="container">
+      <h1>What is The Weather?</h1>
+
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Enter the name of a city.</label>
+          <input type="text" class="form-control" id="city" name="city" placeholder="London, Moscow" value="<?php echo $_GET['city'];?>">
+          
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
+      <div id="weather"><?
+        if ($weather){
+          echo '<div class="alert alert-success" role="alert">'
+          . $weather .
+'</div>';
+        } else if ($error){
+          echo '<div class="alert alert-danger" role="alert">'
+          . $error .
+'</div>';
+        }
+
+      ?></div>
     </div>
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-
-
-    <script type="text/javascript">
-    	
-    	$("form").submit(function(e){
-    		var error = "";
-
-    		if ($("#email").val() == ""){
-    			error += "Email обязателен<br>";
-    		}
-
-    		if ($("#subject").val() == ""){
-    			error += "Тема письма обязательна<br>";
-    		}
-
-    		if ($("#text_mess").val() == ""){
-    			error += "Текст письма обязателен<br>";
-    		}
-
-    		if (error != ""){
-    			$("#error").html('<div class="alert alert-danger" role="alert"> <p>У вас ошибка:</p>' + error + '</div>');
-    				return false;
-    		}
-
-    		else {
-    			$("#error").css("display", "none");
-    			return true;
-    		}
-    	});
-
-    </script>
 
   </body>
 </html>
